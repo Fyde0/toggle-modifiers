72c57a77d01e0f3066a0eaa2721506d24fd820a6
Federico <fyde90@gmail.com>
2024-09-21 15:35:58 +0200

    added code to change padding for later

--- togglemodifiers@fyde0/extension.js
+++ togglemodifiers@fyde0/extension.js
@@ -47,8 +47,10 @@ const Indicator = GObject.registerClass(
                 return Clutter.EVENT_PROPAGATE
             }
 
+            // toggle modifier status
             this.active = !this.active
 
+            // run command
             if (this.active) {
                 this._subprocess(modifiers[this.modifier].commandDown)
                 this._icon.icon_name = modifiers[this.modifier].iconKeyDown
@@ -63,17 +65,48 @@ const Indicator = GObject.registerClass(
             try {
                 Gio.Subprocess.new(command, Gio.SubprocessFlags.NONE)
             } catch (e) {
-                Main.notify('Toggle error.')
+                Main.notify("Toggle error.")
             }
         }
     })
 
 export default class ToggleModifier extends Extension {
+
+    // needed for applying css
+    // https://gitlab.com/p91paul/status-area-horizontal-spacing-gnome-shell-extension/-/blob/95391c9d1d51b8df1082794ee584d931a2c3610f/status-area-horizontal-spacing@mathematical.coffee.gmail.com/extension.js
+    _refreshActor(actor) {
+        let oldClass = actor.get_style_class_name();
+        actor.set_style_class_name('dummy-class-unlikely-to-exist-status-area-horizontal-spacing');
+        actor.set_style_class_name(oldClass);
+    }
+
     enable() {
+
+        // buttons container
+        this._box = new St.BoxLayout({ name: 'modifiersBox' })
+        Main.panel.add_child(this._box)
+
+        // set padding
+        // const style = "-minimum-hpadding: 1px; -natural-hpadding: 1px;"
+        const style = ""
+
+        // create button
         this._ctrlIndicator = new Indicator("ctrl")
+        // get current style and add padding to it
+        const ctrlStyle = this._ctrlIndicator.get_style()
+        this._ctrlIndicator.set_style(style + " " + ctrlStyle)
+        // refresh to apply style
+        this._refreshActor(this._ctrlIndicator)
+
         this._shiftIndicator = new Indicator("shift")
-        Main.panel.addToStatusArea(this.uuid + "-ctrl", this._ctrlIndicator)
-        Main.panel.addToStatusArea(this.uuid + "-shift", this._shiftIndicator)
+        const shiftStyle = this._shiftIndicator.get_style()
+        this._shiftIndicator.set_style(style + " " + shiftStyle)
+        this._refreshActor(this._shiftIndicator)
+
+        // add to bar
+        // (id, thing to add, position, container)
+        Main.panel.addToStatusArea(this.uuid + "-ctrl", this._ctrlIndicator, 0, this._box)
+        Main.panel.addToStatusArea(this.uuid + "-shift", this._shiftIndicator, 1, this._box)
     }
 
     disable() {
@@ -81,5 +114,7 @@ export default class ToggleModifier extends Extension {
         this._ctrlIndicator = null
         this._shiftIndicator.destroy()
         this._shiftIndicator = null
+        this._box.destroy()
+        this._box = null
     }
 }
